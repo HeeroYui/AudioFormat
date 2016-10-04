c26048fd149b319f5357af5a960c6757d1216b1a
Edouard DUPIN <yui.heero@gmail.com>
2016-10-04 23:41:29 +0200

    [DEV] Update new lutin 2.2.0 (no legacy support)

--- lutin_audio-test.py
+++ lutin_audio-test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import lutin.module as module
+import lutin.debug as debug
 import lutin.tools as tools
 
 
@@ -24,25 +24,28 @@ def get_compagny_name():
 def get_maintainer():
 	return "authors.txt"
 
-def create(target, module_name):
-	my_module = module.Module(__file__, module_name, get_type())
+def configure(target, my_module):
 	my_module.add_src_file([
-		'test/base.cpp',
-		'test/main.cpp',
-		'test/test_double.cpp',
-		'test/test_float.cpp',
-		'test/test_int16_16.cpp',
-		'test/test_int16_32.cpp',
-		'test/test_int24_24.cpp',
-		'test/test_int24_32.cpp',
-		'test/test_int32_32.cpp',
-		'test/test_int32_64.cpp',
-		'test/test_int64_64.cpp',
-		'test/test_int8_16.cpp',
-		'test/test_int8_8.cpp'
-		])
-	my_module.add_depend(['audio', 'gtest', 'test-debug'])
-	return my_module
+	    'test/base.cpp',
+	    'test/main.cpp',
+	    'test/test_double.cpp',
+	    'test/test_float.cpp',
+	    'test/test_int16_16.cpp',
+	    'test/test_int16_32.cpp',
+	    'test/test_int24_24.cpp',
+	    'test/test_int24_32.cpp',
+	    'test/test_int32_32.cpp',
+	    'test/test_int32_64.cpp',
+	    'test/test_int64_64.cpp',
+	    'test/test_int8_16.cpp',
+	    'test/test_int8_8.cpp'
+	    ])
+	my_module.add_depend([
+	    'audio',
+	    'gtest',
+	    'test-debug'
+	    ])
+	return True
 
 
 
--- lutin_audio.py
+++ lutin_audio.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import lutin.module as module
+import lutin.debug as debug
 import lutin.tools as tools
 
 def get_type():
@@ -23,45 +23,46 @@ def get_maintainer():
 def get_version():
 	return "version.txt"
 
-def create(target, module_name):
-	my_module = module.Module(__file__, module_name, get_type())
+def configure(target, my_module):
 	my_module.add_src_file([
-		'audio/debug.cpp',
-		'audio/channel.cpp',
-		'audio/format.cpp',
-		'audio/Time.cpp',
-		'audio/Duration.cpp',
-		'audio/int8_8_t.cpp',
-		'audio/int8_16_t.cpp',
-		'audio/int16_16_t.cpp',
-		'audio/int16_32_t.cpp',
-		'audio/int24_24_t.cpp',
-		'audio/int24_32_t.cpp',
-		'audio/int32_32_t.cpp',
-		'audio/int32_64_t.cpp',
-		'audio/int64_64_t.cpp',
-		'audio/float_t.cpp',
-		'audio/double_t.cpp'
-		])
+	    'audio/debug.cpp',
+	    'audio/channel.cpp',
+	    'audio/format.cpp',
+	    'audio/Time.cpp',
+	    'audio/Duration.cpp',
+	    'audio/int8_8_t.cpp',
+	    'audio/int8_16_t.cpp',
+	    'audio/int16_16_t.cpp',
+	    'audio/int16_32_t.cpp',
+	    'audio/int24_24_t.cpp',
+	    'audio/int24_32_t.cpp',
+	    'audio/int32_32_t.cpp',
+	    'audio/int32_64_t.cpp',
+	    'audio/int64_64_t.cpp',
+	    'audio/float_t.cpp',
+	    'audio/double_t.cpp'
+	    ])
 	my_module.add_header_file([
-		'audio/types.hpp',
-		'audio/channel.hpp',
-		'audio/format.hpp',
-		'audio/Time.hpp',
-		'audio/Duration.hpp',
-		'audio/int8_8_t.hpp',
-		'audio/int8_16_t.hpp',
-		'audio/int16_16_t.hpp',
-		'audio/int16_32_t.hpp',
-		'audio/int24_24_t.hpp',
-		'audio/int24_32_t.hpp',
-		'audio/int32_32_t.hpp',
-		'audio/int32_64_t.hpp',
-		'audio/int64_64_t.hpp',
-		'audio/float_t.hpp',
-		'audio/double_t.hpp'
-		])
-	my_module.add_depend(['etk'])
-	my_module.add_path(tools.get_current_path(__file__))
-	return my_module
+	    'audio/types.hpp',
+	    'audio/channel.hpp',
+	    'audio/format.hpp',
+	    'audio/Time.hpp',
+	    'audio/Duration.hpp',
+	    'audio/int8_8_t.hpp',
+	    'audio/int8_16_t.hpp',
+	    'audio/int16_16_t.hpp',
+	    'audio/int16_32_t.hpp',
+	    'audio/int24_24_t.hpp',
+	    'audio/int24_32_t.hpp',
+	    'audio/int32_32_t.hpp',
+	    'audio/int32_64_t.hpp',
+	    'audio/int64_64_t.hpp',
+	    'audio/float_t.hpp',
+	    'audio/double_t.hpp'
+	    ])
+	my_module.add_depend([
+	    'etk'
+	    ])
+	my_module.add_path(".")
+	return True
 
