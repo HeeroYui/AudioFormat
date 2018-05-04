2ccae3bbf6ef24fa3b294d9edb428972e400e33e
Edouard DUPIN <yui.heero@gmail.com>
2018-05-04 22:02:50 +0200

    [DEBUG] add missing file

---
+++ audio/types.cpp
@@ -0,0 +1,15 @@
+/** @file
+ * @author Edouard DUPIN 
+ * @copyright 2011, Edouard DUPIN, all right reserved
+ * @license MPL v2.0 (see license file)
+ */
+#include <audio/types.hpp>
+
+const int64_t INT24_MIN = 0XFFFFFFFFFF800000;
+const int64_t INT24_MAX = 0X00000000007FFFFF;
+
+const int64_t INT40_MIN = 0XFFFFFF8000000000;
+const int64_t INT40_MAX = 0X0000007FFFFFFFFF;
+
+const int64_t INT56_MIN = 0XFF80000000000000;
+const int64_t INT56_MAX = 0X007FFFFFFFFFFFFF;
